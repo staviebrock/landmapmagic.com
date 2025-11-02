87048853dd29dd3f64ff96f3df92089b1a362048
Carl Lippert <carl.lippert@gmail.com>
2025-11-02 14:04:02 -0300

    nice google example

--- examples/src/main.tsx
+++ examples/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BasicExample from './BasicExample';
+import BasicExample from './GreenFieldExample';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
